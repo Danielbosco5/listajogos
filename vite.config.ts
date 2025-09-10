38bb29b4706ff00aa56bd9188d316cf624ab7a74
SEDUC Edu <edu@seduc.go.gov.br>
2025-09-10 16:46:31 -0300

    Corrige definição das variáveis de ambiente do Supabase no build do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      define: {
+        'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+        'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
       }
     };
 });
